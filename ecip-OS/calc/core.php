5b86ad83a67649af9d4f4e16a32b6c477b23667a
PressTpro <95140308+PressTpro@users.noreply.github.com>
2023-01-22 19:25:33 -0500

    Create core.php

---
+++ ecip-OS/calc/core.php
@@ -0,0 +1,9 @@
+<?php
+$name = "PressCalc";
+include '../mustlogin.php';
+include '../css.php';
+echo '<a href="/ecip-OS/desktop.php">Return to Desktop</a>';
+?>
+<title><?php $name; ?></title>
+<body class="loggedin">
+    
